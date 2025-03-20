385d1aa5acc8f3fe0acf366e56b693dc70aacaef
Tony Daange <tanmaydaange@gmail.com>
2025-03-20 16:19:47 +0530

    day 8

---
+++ pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div> hello about 
+        <h1>About</h1>
+        <p>This is the about page</p>
+
+    </div>
+  )
+}
+
+export default About
