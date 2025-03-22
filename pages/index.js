0f05d83d2bd28158cecbdb54c5f43fae4fcf9c9b
Tony Daange <tanmaydaange@gmail.com>
2025-03-22 08:24:57 +0530

    Tutorial 10 Part 1

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,11 @@ import Image from 'next/image'
 import Script from 'next/script'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+import styles1 from '../styles/Home1.module.css'
+import styles2 from '../styles/Home2.module.css'
 
 export default function Home() {
+  console.log(styles)
   return (
     <div className={styles.container}>
       <Head>
@@ -30,7 +33,7 @@ export default function Home() {
         <p className={styles.description}>
           A blog for hunting coders by a hunting coder
         </p>
-        <div className="blogs">
+        <div className={`${styles1.con} ${styles2.con}`}>
           <h2>Popular blogs</h2>
           <div className="blogItem">
             <h3>How to Learn JavaScript in 2025?</h3>
