12740e5d681981ef1d242dceaedd34d043090361
Tony Daange <tanmaydaange@gmail.com>
2025-06-17 21:04:10 +0530

    Tutorial 22

--- pages/blog.js
+++ pages/blog.js
@@ -1,26 +1,41 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from '../styles/Blog.module.css'
 import Link from 'next/link'
 
-const Blog = () => {
+const Blog = (props) => {
+  console.log(props)
+  const [blogs, setBlogs] = useState(props.allBlogs || [])
+  // useEffect(() => {
+  //   fetch('http://localhost:3000/api/blogs').then((a) => {
+  //     return a.json();
+  //   })
+  //     .then((parsed) => {
+  //       setBlogs(parsed)
+  //     })
+  // }, [])
+
   return (
     <main className={styles.main}>
-      <div>
-        <Link href={'/blogpost/how-to-learn-js'}>
-          <h2 >How to Learn JavaScript in 2025?</h2>
-        </Link>
-        <p>JavaScript is the language used to logic for the web</p>
-      </div>
-      <div className="blogItem">
-        <h2>How to Learn JavaScript in 2025?</h2>
-        <p>JavaScript is the language used to logic for the web</p>
-      </div>
-      <div className="blogItem">
-        <h2>How to Learn JavaScript in 2025?</h2>
-        <p>JavaScript is the language used to logic for the web</p>
-      </div>
+      {blogs.map((blogitem) => {
+        return (
+          <div key={blogitem.slug} className={styles.blogitem}>
+            <Link href={`/blogpost/${blogitem.slug}`}>
+              <h2 className={styles.blogItemTitle}>{blogitem.title}</h2>
+              <p>{blogitem.content.substr(0, 150)}...</p>
+            </Link>
+          </div>
+        )
+      })}
     </main>
   )
+}
+export async function getServerSideProps(context) {
+  let data = await fetch('http://localhost:3000/api/blogs')
+  let allBlogs = await data.json()
+
+  return {
+    props: {allBlogs}, // Will be passed to the page component
+  };
 }
 
 export default Blog
